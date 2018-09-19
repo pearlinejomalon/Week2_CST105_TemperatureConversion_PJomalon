a6807416fa149dd4a6aebb968ef49a75a01f8350
pearlinejomalon <43321994+pearlinejomalon@users.noreply.github.com>
2018-09-18 23:36:16 -0500

    Add files via upload

---
+++ ConvertTemperature.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ConvertTemperature {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Enter a Fahrenheit temperature:");
+		Scanner scanner=new Scanner(System.in);
+		
+		float fahrenheit=scanner.nextFloat();
+		
+		float celsius=(float) ((5 *(fahrenheit - 32.0)) / 9.0);
+		System.out.print(fahrenheit + "F" + "is equivalent to" + celsius + "C");
+		
+		
+		System.out.print("Enter a Celsius temperature:");
+			
+		Scanner scanner1 = new Scanner(System.in);
+		float celsius1=scanner1.nextFloat();
+			
+		float fahrenheit1=(float) ((celsius1*(9.0 / 5) + 32.0));
+		System.out.print(celsius1 + "C" + "is equivalent to" + fahrenheit1 + "F");
+			
+		scanner.close();
+		
+		}
+
+	}
